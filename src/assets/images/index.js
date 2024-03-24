bd23ad9aa88b12c0956c10666738b4937ae17625
Aditya Phasu <adityaphasu919@gmail.com>
2024-03-24 22:41:46 +0530

    Add index files for icons and images

---
+++ src/assets/images/index.js
@@ -0,0 +1,23 @@
+import Logo from './logo.svg';
+import ProfileAvatar from './image-avatar.png';
+import Product1 from './image-product-1.jpg';
+import Product2 from './image-product-2.jpg';
+import Product3 from './image-product-3.jpg';
+import Product4 from './image-product-4.jpg';
+import ProductThumbnail1 from './image-product-1-thumbnail.jpg';
+import ProductThumbnail2 from './image-product-2-thumbnail.jpg';
+import ProductThumbnail3 from './image-product-3-thumbnail.jpg';
+import ProductThumbnail4 from './image-product-4-thumbnail.jpg';
+
+export {
+  Logo,
+  ProfileAvatar,
+  Product1,
+  Product2,
+  Product3,
+  Product4,
+  ProductThumbnail1,
+  ProductThumbnail2,
+  ProductThumbnail3,
+  ProductThumbnail4,
+};
