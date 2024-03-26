486c3386f25443fda32a3233c2b8755d59ce5e43
Aditya Phasu <adityaphasu919@gmail.com>
2024-03-26 23:48:23 +0530

    Update App Layout and add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
+import { useContext, useState } from "react";
+import { CartContext } from "./context/CartContext";
+
+import CartModal from "./components/Cart/CartModal";
+import Header from "./components/Header/Header";
+import ProductActions from "./components/Product/ProductActions";
+import ProductDetails from "./components/Product/ProductDetails";
+import ProductLightRoom from "./components/Product/ProductLightRoom";
+import ProductSlider from "./components/Product/ProductSlider/ProductSlider";
+import ProductThumbnailSelector from "./components/Product/ProductThumbnail/ProductThumbnailSelector";
+
+import "./index.css";
+
 function App() {
-  return <div className=''></div>;
+  const { isCartOpen } = useContext(CartContext);
+
+  const [isLightRoomOpen, setIsLightRoomOpen] = useState(false);
+
+  const handleLightRoom = () => {
+    setIsLightRoomOpen(true);
+  };
+
+  return (
+    <>
+      <Header />
+      <main className="pb-20 lg:mx-auto lg:grid lg:max-w-[69rem] lg:grid-cols-2 lg:border-t lg:px-11 lg:py-[5.5rem] xl:gap-20 ">
+        {isCartOpen && <CartModal />}
+        <div>
+          <ProductSlider className="lg:hidden" />
+          <ProductThumbnailSelector onClick={handleLightRoom} />
+          {isLightRoomOpen && (
+            <ProductLightRoom
+              isLightRoomOpen={isLightRoomOpen}
+              setIsLightRoomOpen={setIsLightRoomOpen}
+            />
+          )}
+        </div>
+        <div className="lg:flex lg:flex-col">
+          <ProductDetails />
+          <ProductActions />
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;
