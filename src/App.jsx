99727831df4dc2b787af70e37fbba66b99618e9f
Aditya Phasu <adityaphasu919@gmail.com>
2024-03-28 23:36:17 +0530

    Refactor CartContext and App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useContext, useState } from "react";
-import { CartContext } from "./context/CartContext";
+import { useState } from "react";
+import { useCart } from "./context/CartContext";
 
 import CartModal from "./components/Cart/CartModal";
 import Header from "./components/Header/Header";
@@ -12,7 +12,7 @@ import ProductThumbnailSelector from "./components/Product/ProductThumbnail/Prod
 import "./index.css";
 
 function App() {
-  const { isCartOpen } = useContext(CartContext);
+  const { isCartOpen } = useCart();
 
   const [isLightRoomOpen, setIsLightRoomOpen] = useState(false);
 
@@ -23,7 +23,7 @@ function App() {
   return (
     <>
       <Header />
-      <main className="pb-20 lg:mx-auto lg:grid lg:max-w-[69rem] lg:grid-cols-2 lg:border-t lg:px-11 lg:py-[5.5rem] xl:gap-20 ">
+      <main className="pb-20 lg:mx-auto lg:grid lg:max-w-[69rem] lg:grid-cols-2 lg:border-t lg:px-11 lg:pb-16 lg:pt-[5.5rem] xl:gap-20 ">
         {isCartOpen && <CartModal />}
         <div>
           <ProductSlider className="lg:hidden" />
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,16 +1,32 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 const CartContext = createContext();
 
-const CartContextProvider = ({ children }) => {
+export const CartContextProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [quantity, setQuantity] = useState(0);
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = () => {
+    if (quantity === 0) return;
+    setCartItems([quantity]);
+  };
+
+  const deleteFromCart = () => {
+    setCartItems([]);
+  };
 
   const values = {
     isCartOpen,
     setIsCartOpen,
+    quantity,
+    setQuantity,
+    cartItems,
+    addToCart,
+    deleteFromCart,
   };
 
   return <CartContext.Provider value={values}>{children}</CartContext.Provider>;
 };
 
-export { CartContext, CartContextProvider };
+export const useCart = () => useContext(CartContext);
