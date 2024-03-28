f0e04198acd59deedd51d5b24ebbc43894fd17e5
Aditya Phasu <adityaphasu919@gmail.com>
2024-03-28 23:37:41 +0530

    Add onClick functionality to QuantityButton and update QuantitySelector to use CartContext

--- src/components/QuantitySelector/QuantityButton.jsx
+++ src/components/QuantitySelector/QuantityButton.jsx
@@ -1,6 +1,7 @@
-const QuantityButton = ({ icon }) => {
+const QuantityButton = ({ icon, onClick }) => {
   return (
     <button
+      onClick={onClick}
       className="grid w-[3.75rem] place-items-center bg-light-grayish-blue py-[1.35rem] transition hover:bg-pale-orange lg:w-11 lg:py-[1.43rem]"
       aria-label={icon}>
       <img src={icon} alt="" />
--- src/components/QuantitySelector/QuantitySelector.jsx
+++ src/components/QuantitySelector/QuantitySelector.jsx
@@ -1,12 +1,24 @@
 import { Minus, Plus } from "../../assets/icons";
+import { useCart } from "../../context/CartContext";
 import QuantityButton from "./QuantityButton";
 
 const QuantitySelector = () => {
+  const { quantity, setQuantity } = useCart();
+
+  const handlePlus = () => {
+    setQuantity((prev) => prev + 1);
+  };
+
+  const handleMinus = () => {
+    if (quantity <= 0) return;
+    setQuantity((prev) => prev - 1);
+  };
+
   return (
     <div className="mt-0.5 flex justify-between overflow-hidden rounded-lg bg-light-grayish-blue font-bold lg:flex-row">
-      <QuantityButton icon={Minus} />
-      <span className="self-center lg:pl-7 lg:pr-[1.9rem]">0</span>
-      <QuantityButton icon={Plus} />
+      <QuantityButton icon={Minus} onClick={handleMinus} />
+      <span className="flex items-center justify-center lg:w-[4.2rem]">{quantity}</span>
+      <QuantityButton icon={Plus} onClick={handlePlus} />
     </div>
   );
 };
