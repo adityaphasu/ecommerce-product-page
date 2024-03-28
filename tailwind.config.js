b55793f21ddd2cb1eec1b3228c87ee636710fe67
Aditya Phasu <adityaphasu919@gmail.com>
2024-03-28 23:36:24 +0530

    Add custom boxShadow for cart

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ export default {
     extend: {
       boxShadow: {
         custom: "0 15px 38px -15px",
+        cart: "0 0 10px 5px rgba(0, 0, 0, 0.1)",
       },
       colors: {
         orange: "hsl(26, 100%, 55%)",
