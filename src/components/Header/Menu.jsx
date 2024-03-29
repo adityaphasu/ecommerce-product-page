7ec6fd50fae81c3b3362311c344ba42a8ed67402
Aditya Phasu <adityaphasu919@gmail.com>
2024-03-29 23:14:03 +0530

    Add hover effect and selected indicator to menu items

--- src/components/Header/Menu.jsx
+++ src/components/Header/Menu.jsx
@@ -6,10 +6,13 @@ import { links } from "../../constants";
 
 const Menu = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState(null);
 
   const handleMenuClick = () => {
     setIsOpen((prev) => !prev);
   };
+
+  console.log(selected);
 
   return (
     <>
@@ -32,11 +35,16 @@ const Menu = () => {
           "fixed inset-0 z-20 flex h-full w-4/6 flex-col gap-4 bg-white px-6 pt-[5.6rem] transition-all duration-300 lg:static lg:flex-row lg:gap-8 lg:p-0",
           isOpen ? "-left-0" : "-left-[50rem]"
         )}>
-        {links.map((link) => (
+        {links.map((link, idx) => (
           <li
             key={link}
-            className="text-lg font-bold leading-relaxed text-very-dark-blue lg:text-base lg:font-normal lg:tracking-[-0.02em]">
+            onMouseEnter={() => setSelected(idx)}
+            onMouseLeave={() => setSelected(null)}
+            className="relative text-lg font-bold leading-relaxed text-dark-grayish-blue hover:text-very-dark-blue lg:text-base lg:font-normal lg:tracking-[-0.02em]">
             <a href="/">{link}</a>
+            {selected === idx && (
+              <span className="absolute -bottom-[3.1rem] block h-1 w-full bg-orange" />
+            )}
           </li>
         ))}
       </ul>
