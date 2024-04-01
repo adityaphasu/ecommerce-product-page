b9fad919c3e72e3e522711d5e288095f08cb5c8c
Aditya Phasu <adityaphasu919@gmail.com>
2024-04-01 22:40:21 +0530

    Add theme color meta tag and PWA manifest file

---
+++ public/manifest.js
@@ -0,0 +1,34 @@
+export const manifest = {
+  theme_color: "#f69435",
+  background_color: "#f69435",
+  display: "standalone",
+  scope: "/",
+  start_url: "/",
+  name: "Ecommerce Product Page",
+  short_name: "Sneakers",
+  description:
+    "An Ecommerce product page for Sneakers. This is a Frontend Mentor challenge solution.",
+  icons: [
+    {
+      src: "/icon-192x192.png",
+      sizes: "192x192",
+      type: "image/png",
+    },
+    {
+      src: "/icon-256x256.png",
+      sizes: "256x256",
+      type: "image/png",
+    },
+    {
+      src: "/icon-384x384.png",
+      sizes: "384x384",
+      type: "image/png",
+    },
+    {
+      src: "/icon-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
+      purpose: "any maskable",
+    },
+  ],
+};
